010f71c4f3c46bd5e83b43084d952fd7c65383fe
dimitri20 <dimitri.gulua@geolab.edu.ge>
2021-07-21 11:00:12 +0400

    add input group to contact page

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -143,47 +143,7 @@
     @yield("content")
 
 
-    <footer id="footer">
-        <div class="container-main">
-            <div id="footer-inner-container">
-                <div class="row">
-                    <div class="col">
-                        <div class="d-flex flex-row">
-                            <div class="footer-contents-container d-flex flex-row">
-                                <div class="footer-content-item">
-                                    <a href="#">
-                                        <img src="{{ asset("storage/icons/google-maps.svg") }}" alt="">
-                                    </a>
-
-                                    <a href="#">
-                                        მისამართი N2 ქუჩა 2912
-                                    </a>
-                                </div>
-
-                                <div class="footer-content-item">
-                                    <a href="#">
-                                        <img src="{{ asset("storage/icons/telephone.svg") }}" alt="">
-                                    </a>
-
-                                    <a href="#">
-                                        +995 555 555 555
-                                    </a>
-                                </div>
-                            </div>
-    
-                            <div class="ms-auto">
-                                <div class="footer-content-item">
-                                    <a href="#">
-                                        &#169; ყველა უფლება დაცულია
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    @include('assets.footer')
 
     <script src="{{asset("js/app.js")}}"></script>
     {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
